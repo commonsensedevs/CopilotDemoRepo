ad0787af245e5f48f009c163ddf2126247f24770
Gomti Mehta <gomtiprofessional@gmail.com>
2023-09-09 11:22:56 -0500

    Added unit tests

---
+++ UnitTests/CategoryRepositoryCreateTests.cs
@@ -0,0 +1,42 @@
+﻿using Core;
+using DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class CategoryRepositoryCreateTests
+    {
+        [Fact]
+        public void CreateNewCategoryTest()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<CopilotDemoDBContext>()
+                .UseInMemoryDatabase(databaseName: "CreateNewCategoryTest")
+                .Options;
+
+            using (var context = new CopilotDemoDBContext(options))
+            {
+                var categoryRepository = new CategoryRepository(context);
+                var category = new Category()
+                {
+                    Name = "Test Category",
+                    Description = "Test Description"
+                };
+
+                // Act
+                categoryRepository.Add(category);
+
+                // Assert
+                Assert.Equal(1, context.Categories.Count());
+                Assert.Equal("Test Category", context.Categories.Single().Name);
+                Assert.Equal("Test Description", context.Categories.Single().Description);
+            }
+
+        }
+    }
+}
---
+++ UnitTests/CategoryRepositoryGetCategoriesTests.cs
@@ -0,0 +1,47 @@
+﻿using Core;
+using DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class CategoryRepositoryGetCategoriesTests
+    {
+        [Fact]
+        public void GetAllCategoriesTest()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<CopilotDemoDBContext>()
+                .UseInMemoryDatabase(databaseName: "GetAllCategoriesTest")
+                .Options;
+
+            using (var context = new CopilotDemoDBContext(options))
+            {
+                var categoryRepository = new CategoryRepository(context);
+                var category1 = new Category()
+                {
+                    Name = "Test Category",
+                    Description = "Test Description"
+                };
+                var category2 = new Category()
+                {
+                    Name = "Test Category",
+                    Description = "Test Description"
+                };
+
+                // Act
+                categoryRepository.Add(category1);
+                categoryRepository.Add(category2);
+
+                var categories = categoryRepository.GetAll();
+                // Assert
+                Assert.Equal(2, categories.Count());
+            }
+
+        }
+    }
+}
